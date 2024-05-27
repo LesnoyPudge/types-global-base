60c04d707a99694df9ec6a8cb81c971618cadaeb
LesnoyPudge <chuykin.a01@gmail.com>
2024-05-27 16:32:48 +0500

    'update'

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="node_modules/@total-typescript/ts-reset/dist/dom.d.ts" />
-/// <reference path="node_modules/types-spring/sources/index.d.ts" />
+/// <reference path="@total-typescript/ts-reset/dist/dom.d.ts" />
+/// <reference path="types-spring/sources/index.d.ts" />
 /// <reference path="src/index.d.ts" />
